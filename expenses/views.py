fd411fee7c6320577a767c79c072a9dbcdba726b
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2025-05-14 22:16:59 +0900

    週ごとまとめたデータ

--- expenses/views.py
+++ expenses/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render, redirect
-from datetime import date
+from datetime import date, timedelta
+from django.utils import timezone
 from .forms import ExpenseForm
 from .models import Expense
+from collections import defaultdict  # 配列の初期値設定
 
 # Create your views here.
 def index(request):
-    expenses = Expense.objects.filter(user=request.user).order_by('-date')
+    today = timezone.localtime()
+    weekday = (today.weekday()+1)%7
+    print(weekday)
+    start = today - timedelta(days=weekday) 
+    this_week = [(start + timedelta(days=i)).date() for i in range(7)]
+
+    expenses = Expense.objects.filter(
+            user=request.user,
+            date__range=(start, today)
+        ).order_by('-date')
+    
+    daily_amount = defaultdict(int)  # 未定義のインデックスが呼び出された場合にエラーにならない
+    for expense in expenses:
+        daily_amount[expense.date] += expense.amount
+    # daily_amount_total = sorted(daily_amount.items())
+    daily_amount_total = [( date, daily_amount[date] ) for date in this_week]
+
     return render(request, "expenses/index.html",{
         "today": date.today().isoformat,
-        "expenses": expenses
+        "expenses": expenses,
+        "daily_amount_total": daily_amount_total
     })
 
 def create_expense(request):
